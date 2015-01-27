a3f0ce50275875fe3eec5b6170ac7409ac88bb77
Daniel Somerfield <dsomerfi@thoughtworks.com>
2015-01-26 20:17:04 -0800

    First cut of presentation.

--- presentation/src/main/java/Presentation.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.boot.SpringApplication;
-
-public class Presentation {
-    public static void main(String[] args) {
-        SpringApplication.run(Presentation.class, args);
-    }
-}
---
+++ presentation/src/main/java/com/thoughtworks/appsec/xssDemo/presentation/Presentation.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.appsec.xssDemo.presentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Presentation {
+    public static void main(String[] args) {
+        SpringApplication.run(Presentation.class, args);
+    }
+}
