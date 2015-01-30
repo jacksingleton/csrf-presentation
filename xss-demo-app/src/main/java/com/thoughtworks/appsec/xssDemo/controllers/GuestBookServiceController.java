5a79b0fbf3844e3d303f4318d69683f1c1e45710
Daniel Somerfield <dsomerfi@thoughtworks.com>
2015-01-30 12:49:31 -0800

    Fixed delete bug in controller. Added demo exploits.

--- xss-demo-app/src/main/java/com/thoughtworks/appsec/xssDemo/controllers/GuestBookServiceController.java
+++ xss-demo-app/src/main/java/com/thoughtworks/appsec/xssDemo/controllers/GuestBookServiceController.java
@@ -34,22 +34,22 @@ public class GuestBookServiceController {
     @ResponseBody
     @SneakyThrows(IOException.class)
     public DeleteResult deleteAll(HttpServletResponse response, HttpSession session) {
-        validateDelete(response, session);
-        return new DeleteResult(guestBook.clearEntries());
-    }
-
-    private void validateDelete(final HttpServletResponse response, final HttpSession session) throws IOException {
-        UserState userState = (UserState) session.getAttribute(Constants.USER_STATE_SESSION_ATTRIBUTE);
-        if (userState == null || !userState.isLoggedIn()) {
-            response.sendError(HttpServletResponse.SC_FORBIDDEN, "You do not have permission for this operation");
-        }
+        return doDelete(response, session);
     }
 
     @RequestMapping(value="/service/entries",  method = RequestMethod.DELETE, produces = "application/json")
     @ResponseBody
     @SneakyThrows(IOException.class)
     public DeleteResult deleteAllRest(HttpServletResponse response, HttpSession session) {
-        validateDelete(response, session);
+        return doDelete(response, session);
+    }
+
+    private DeleteResult doDelete(final HttpServletResponse response, final HttpSession session) throws IOException {
+        UserState userState = (UserState) session.getAttribute(Constants.USER_STATE_SESSION_ATTRIBUTE);
+        if (userState == null || !userState.isLoggedIn()) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "You do not have permission for this operation");
+            return new DeleteResult(0);
+        }
         return new DeleteResult(guestBook.clearEntries());
     }
 
